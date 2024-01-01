05dfe357e84ba72b535d22d3a17b012cfbcf3438
Swarnim Walavalkar <swarnim.walavalkar@gmail.com>
2024-01-01 22:33:50 +0530

    ♻️ minor refactor ♻️ add dotnev

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,22 +2,22 @@ import constants from "./constants";
 
 export default {
   name: "starter-service",
-  version: process.env.VERSION || "v1",
-  domain: process.env.DOMAIN || "localhost",
-  port: process.env.PORT || 4000,
+  version: process.env.VERSION ?? "v1",
+  domain: process.env.DOMAIN ?? "localhost",
+  port: process.env.PORT ?? 4000,
   db: {
-    user: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD || "postgres",
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 5432,
+    user: process.env.DB_USER ?? "postgres",
+    password: process.env.DB_PASSWORD ?? "postgres",
+    host: process.env.DB_HOST ?? "localhost",
+    port: Number(process.env.DB_PORT) ?? 5432,
   },
   jwt: {
-    tokenSecret: process.env.JWT_TOKEN_SECRET || "supersecretrandomstring",
+    tokenSecret: process.env.JWT_TOKEN_SECRET ?? "supersecretrandomstring",
   },
   redis: {
-    host: process.env.REDIS_HOST || "localhost",
-    port: Number(process.env.REDIS_PORT) || 6379,
-    password: process.env.REDIS_PASSWORD || "",
+    host: process.env.REDIS_HOST ?? "localhost",
+    port: Number(process.env.REDIS_PORT) ?? 6379,
+    password: process.env.REDIS_PASSWORD ?? "",
   },
   constants,
 } as const;
